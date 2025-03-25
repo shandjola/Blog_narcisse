f53981f59ab30c17ea6fb3e8b418096a1155a245
shandjola <maurice.shandjola@gmail.com>
2025-03-25 22:28:09 +0100

    deuxieme commit

--- Blog/Narcise/views.py
+++ Blog/Narcise/views.py
@@ -5,15 +5,21 @@ from django.utils import timezone
 from datetime import timedelta
 
 def liste_articles(request):
-    articles = Article.objects.all()
-    return render(request, 'index.html', {'articles': articles})
+    limit_date = timezone.now() - timedelta(weeks=2)
+    recent_articles = Article.objects.filter(date_publication__gte=limit_date)
+    old_articles = Article.objects.filter(date_publication__lt=limit_date)
+
+    return render(request, 'index.html', {
+        'articles': recent_articles,
+        'old_articles': old_articles,
+    })
 
 def voire_plus(request):
    # Date limite pour considérer un article comme "ancien"
     limit_date = timezone.now() - timedelta(weeks=2)
 
     # Articles anciens (plus de 2 semaines)
-    old_articles = Article.objects.filter(created_at__lt=limit_date)
+    old_articles = Article.objects.filter(date_publication__lt=limit_date)
 
     return render(request, 'detail.html', {
         'old_articles': old_articles,
